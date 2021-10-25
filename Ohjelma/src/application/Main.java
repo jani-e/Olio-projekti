8afc4184ab8e5638cdef91984ae630c9885af280
Noutaja <34986042+Noutaja@users.noreply.github.com>
2021-10-25 16:58:15 +0300

    even more testing

--- Ohjelma/src/application/Main.java
+++ Ohjelma/src/application/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			//aaaaaaaaaaaaa
+			//aaaaaa
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
